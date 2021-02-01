1b2495f0c8c25b744e97c571b454d5b6221ba5ab
wegare123 <56117745+wegare123@users.noreply.github.com>
2021-02-02 00:05:20 +0700

    Add files via upload

--- vmt.sh
+++ vmt.sh
@@ -194,7 +194,7 @@ cat <<EOF>> /etc/crontabs/root
 */1 * * * *  autorekonek-vmt
 # END AUTOREKONEKVMT
 EOF
-sed -i 's/exit 0/ /g' /etc/rc.local
+sed -i '/^$/d' /etc/crontabs/root 2>/dev/null
 /etc/init.d/cron restart
 echo "Enable Suksess"
 sleep 2
