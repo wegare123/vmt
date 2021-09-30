d72c2012ed66f834e93b9200cbf781f1fec4915a
Wega Regianto <56117745+wegare123@users.noreply.github.com>
2021-09-30 09:41:56 +0700

    Add files via upload

--- autorekonek-vmt.sh
+++ autorekonek-vmt.sh
@@ -2,7 +2,7 @@
 #vmt (Wegare)
 route="$(route | grep -i 8.8.8.8 | head -n1 | awk '{print $2}')" 
 route2="$(route | grep -i 10.0.0.2 | head -n1 | awk '{print $2}')" 
-route3="$(lsof -i | grep -i v2ray | grep -i 1080 | grep -i listen)" 
+route3="$(netstat -plantu | grep -i v2ray | grep -i 1080 | grep -i listen)" 
 echo $route
 	if [[ -z $route2 ]]; then
 		   (printf '3\n'; sleep 15; printf '\n') | vmt	
